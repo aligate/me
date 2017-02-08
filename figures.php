40f3768a17c58793d23010ff531c571810666c2f
aligate <aligate26@gmail.com>
2017-02-08 13:18:40 +0100

    Add files via upload

---
+++ figures.php
@@ -0,0 +1,56 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+//Числа Фибоначчи
+
+$x = $_GET['x'];
+
+
+
+$a = 1;
+ 
+$b = 1;
+
+while($x > $a){
+	
+	$c = $a;
+	
+	$a += $b;
+	
+	$b = $c;
+	
+	}
+
+?>
+
+<h2>Числа Фибоначчи</h2>
+
+<form action="" >
+
+<input type="text" name ="x" placeholder = "Введите число">
+<input type="submit" value="go">
+
+</form>
+
+<?php
+
+if($x!='')
+{
+	
+	if($x == $a)
+	{
+	echo "задуманное число: <b>$x</b> - входит в числовой ряд";
+	
+	}
+ 	
+	if($x < $a)
+	{
+	echo "задуманное число: <b>$x</b> - НЕ входит в числовой ряд";
+
+	}
+} 
+else echo "<p style='color: red;'>Введите задуманное число!</p>";
+
+
+?>
+
+
